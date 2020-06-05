28aeeaf205765df8089c5f1e01a6fe5a7cf1f441
Vince <vin@vinsworld.com>
2020-06-05 14:47:48 -0400

    fix access violation on LookupTag/AutoComplete with nothing selected deal with relative and full filepaths in tags file for peek better error check with ShellExecuteEx in CreateTagsDb() clear editview before redisplay

--- tag_leet_form.cpp
+++ tag_leet_form.cpp
@@ -330,6 +330,7 @@ TL_ERR TagLeetForm::CreateListView(HWND hwnd)
     return TL_ERR_GENERAL;
 
   ::GetClientRect(hwnd, &Rect);
+  // No edit view if doing autocomplete
   if (DoAutoComplete)
     EditHeight = 0;
   else
@@ -685,6 +686,8 @@ std::string ws2s(const std::wstring& wstr)
 
 void TagLeetForm::UpdateEditView()
 {
+    SetWindowTextA(EditHWnd, (LPCSTR)"");
+
     // Get current list view item (need filename and line number)
     int idx = ListView_GetNextItem( LViewHWnd, -1, LVIS_FOCUSED );
     TagList::TagListItem *Item;
@@ -692,22 +695,31 @@ void TagLeetForm::UpdateEditView()
     if (Item == NULL)
       return;
 
-    // Get tagsfilepath (which contains the '\tags' filename, so remove it)
-    char Path[TL_MAX_PATH + 16];
-    int n = (int)::strlen(TList.TagsFilePath);
-    ::memcpy(Path, TList.TagsFilePath, n * sizeof(char));
-    if (Path[n-1] == 's' &&
-        Path[n-2] == 'g' &&
-        Path[n-3] == 'a' &&
-        Path[n-4] == 't' &&
-        Path[n-5] == '\\')
-    {
-      Path[n-5] = '\0';
-    }
-
     int iLine = atoi(Item->ExtLine);
-    std::string strFileToOpen(Path);
-    strFileToOpen += "\\";
+    if ( iLine == NULL )
+        return;
+
+    std::string strFileToOpen("");
+    // relative filename, get tags file path to append
+    if ( Item->FileName[0] == '.' )
+    {
+        // Get tagsfilepath (which contains the '\tags' filename, so remove it)
+        char Path[TL_MAX_PATH + 16];
+        int n = (int)::strlen(TList.TagsFilePath);
+        ::memcpy(Path, TList.TagsFilePath, n * sizeof(char));
+        if (Path[n-1] == 's' &&
+            Path[n-2] == 'g' &&
+            Path[n-3] == 'a' &&
+            Path[n-4] == 't' &&
+            Path[n-5] == '\\')
+        {
+          Path[n-5] = '\0';
+        }
+    
+        std::string strFileToOpen(Path);
+        strFileToOpen += "\\";
+    }
+    // otherwise assume full file path/name
     strFileToOpen += Item->FileName;
 
     // open the file for reading
@@ -944,6 +956,7 @@ LRESULT TagLeetForm::WndProc( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
             case VK_RETURN:
             case VK_SPACE:
             {
+              // tooltip
               if ( DoAutoComplete )
               {
                 LVITEM LvItem;
