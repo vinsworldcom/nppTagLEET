6f120f50dc6f3c1e98781cec758a24f4c4510b5a
VinsWorldcom <vin@vinsworld.com>
2021-05-18 17:12:58 -0400

    bump version

--- resource.h
+++ resource.h
@@ -1,7 +1,7 @@
 #define IDR_ICONS   101
 
-#define VERSION_WORDS    1,3,9,3
-#define VERSION_TAGLEET "1.3.9.3"
+#define VERSION_WORDS    1,3,9,4
+#define VERSION_TAGLEET "1.3.9.4"
 
 #ifndef IDC_STATIC
 #define IDC_STATIC  -1
