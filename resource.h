9bf599e6b19407008a8f4b4fa34253acb47a4439
VinsWorldcom <vin@vinsworld.com>
2022-01-16 21:27:23 -0500

    bump version

--- resource.h
+++ resource.h
@@ -1,7 +1,7 @@
 #define IDR_ICONS   101
 
-#define VERSION_WORDS    1,3,9,5
-#define VERSION_TAGLEET "1.3.9.5"
+#define VERSION_WORDS    1,3,9,6
+#define VERSION_TAGLEET "1.3.9.6"
 
 #ifndef IDC_STATIC
 #define IDC_STATIC  -1
