2fc41282e966203c6b14e3a8bc8610af0d2868b5
VinsWorldcom <vin@vinsworld.com>
2022-02-17 17:02:07 -0500

    update build number

--- resource.h
+++ resource.h
@@ -1,7 +1,7 @@
 #define IDR_ICONS   101
 
-#define VERSION_WORDS    1,3,10,1
-#define VERSION_TAGLEET "1.3.10.1"
+#define VERSION_WORDS    1,3,10,2
+#define VERSION_TAGLEET "1.3.10.2"
 
 #ifndef IDC_STATIC
 #define IDC_STATIC  -1
